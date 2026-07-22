b419c33898a2de428599de7ce47135f450c74ad6
NeitherDucks <jolan92@gmail.com>
2026-07-21 19:31:41 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+// Support configuring Bevy lints within code.
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
+use bevy::{asset::AssetMetaCheck, prelude::*};
+
+fn main() -> AppExit {
+    App::new().add_plugins(AppPlugin).run()
+}
+
+pub struct AppPlugin;
+
+impl Plugin for AppPlugin {
+    fn build(&self, app: &mut App) {
+        // Add Bevy plugins.
+        app.add_plugins((
+            DefaultPlugins
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics on web build on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Window {
+                        title: "GMTK 2026".to_string(),
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }
+                    .into(),
+                    ..default()
+                }),
+            bevy_framepace::FramepacePlugin,
+            bevy_rand::prelude::EntropyPlugin::<bevy_prng::ChaCha20Rng>::default(),
+        ));
+
+        #[cfg(feature = "dev")]
+        app.add_plugins((
+            bevy_inspector_egui::bevy_egui::EguiPlugin::default(),
+            bevy_inspector_egui::quick::WorldInspectorPlugin::default(),
+        ));
+    }
+}
